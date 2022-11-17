c185254f07f6d644ca88e49cfc980abf3090acc3
Mikhail Kislenko <citizen.of.atlantis@outlook.com>
2022-11-17 18:00:35 +0400

    Added first compile test

--- src/Citizen17.DartSass.Tests/DartSassCompilerTests.cs
+++ src/Citizen17.DartSass.Tests/DartSassCompilerTests.cs
@@ -8,4 +8,36 @@ public class DartSassCompilerTests
     {
         Assert.IsNotNull(new DartSassCompiler());
     }
+
+    [TestMethod]
+    public void CreateWithWrongPathTest()
+    {
+        Assert.IsNotNull(new DartSassCompiler("/not/real/path.exe"));
+    }
+
+    [TestMethod]
+    public async Task CompileFileWithDefaultOptionsTest()
+    {
+        var compiler = new DartSassCompiler();
+
+        var result = await compiler.CompileAsync("./TestSourceFiles/TestSheet.scss");
+
+        Assert.IsNotNull(result.Code);
+        Assert.IsTrue(!result.Debug.Any());
+        Assert.IsTrue(!result.DeprecationWarnings.Any());
+        Assert.IsTrue(!result.Warnings.Any());
+
+        var expectedResult = """
+            .my-class {
+              color: red;
+            }
+            .my-class.active {
+              color: green;
+            }
+            .my-class .text {
+              text-align: right;
+            }
+            """;
+        Assert.AreEqual(expectedResult, result.Code.Trim());
+    }
 }
