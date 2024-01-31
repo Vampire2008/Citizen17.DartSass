ceae9609b051deba46e830c9252a3efa4aef10c9
Mikhail Kislenko <citizen.of.atlantis@outlook.com>
2024-01-31 09:48:55 +0400

    Updated version of Dart Sass Added new Native platforms Updated download script

--- getDartSassExecutables.ps1
+++ getDartSassExecutables.ps1
@@ -7,74 +7,43 @@ if (Test-Path "./dart-sass") {
 }
 New-Item -ItemType Directory -Force $TempDir
 
-try {
-	Invoke-WebRequest -Uri "$($DownloadUrl)dart-sass-$($DartSassVersion)-windows-x64.zip" -OutFile "$TempDir/win-x64.zip"
-	Expand-Archive -Path "$TempDir/win-x64.zip" -DestinationPath "./dart-sass/win-x64" -Force
-} catch {
-	Write-Host "Problem with Windows x64";
-	Write-Host $_
+function downloadAndExtract($name, $title) {
+	try {
+		Invoke-WebRequest -Uri "$($DownloadUrl)dart-sass-$($DartSassVersion)-$($name)" -OutFile "$TempDir/$name"
+		if ($name.EndsWith("tar.gz")) {
+			$dirName = [System.IO.Path]::GetFileNameWithoutExtension([System.IO.Path]::GetFileNameWithoutExtension($name));
+			New-Item -ItemType Directory -Force "./dart-sass/$dirName"
+			tar -xf "$TempDir/$name" -C "./dart-sass/$dirName"
+		}
+		else {
+			$dirName = [System.IO.Path]::GetFileNameWithoutExtension($name)
+			Expand-Archive -Path "$TempDir/$name" -DestinationPath "./dart-sass/$dirName" -Force
+		}
+	} catch {
+		Write-Host "Problem with $title";
+		Write-Host $_
+	}
 }
 
-try {
-	Invoke-WebRequest -Uri "$($DownloadUrl)dart-sass-$($DartSassVersion)-windows-ia32.zip" -OutFile "$TempDir/win-ia32.zip"
-	Expand-Archive -Path "$TempDir/win-ia32.zip" -DestinationPath "./dart-sass/win-ia32" -Force
-} catch {
-	Write-Host "Problem with Windows x86";
-	Write-Host $_
-}
+downloadAndExtract "windows-x64.zip"         "Windows x64"
+downloadAndExtract "windows-ia32.zip"        "Windows x86"
 
-try {
-	Invoke-WebRequest -Uri "$($DownloadUrl)dart-sass-$($DartSassVersion)-linux-x64.tar.gz" -OutFile "$TempDir/linux-x64.tar.gz"
-	New-Item -ItemType Directory -Force "./dart-sass/linux-x64"
-	tar -xf "$TempDir/linux-x64.tar.gz" -C "./dart-sass/linux-x64"
-} catch {
-	Write-Host "Problem with Linux x64";
-	Write-Host $_
-}
+downloadAndExtract "linux-x64.tar.gz"        "Linux x64"
+downloadAndExtract "linux-ia32.tar.gz"       "Linux x86"
+downloadAndExtract "linux-arm.tar.gz"        "Linux arm"
+downloadAndExtract "linux-arm64.tar.gz"      "Linux arm64"
 
-try {
-	Invoke-WebRequest -Uri "$($DownloadUrl)dart-sass-$($DartSassVersion)-linux-ia32.tar.gz" -OutFile "$TempDir/linux-ia32.tar.gz"
-	New-Item -ItemType Directory -Force "./dart-sass/linux-ia32"
-	tar -xf "$TempDir/linux-ia32.tar.gz" -C "./dart-sass/linux-ia32"
-} catch {
-	Write-Host "Problem with Linux x86";
-	Write-Host $_
-}
+downloadAndExtract "linux-musl-x64.tar.gz"   "Linux musl x64"
+downloadAndExtract "linux-musl-ia32.tar.gz"  "Linux musl x86"
+downloadAndExtract "linux-musl-arm.tar.gz"   "Linu muslx arm"
+downloadAndExtract "linux-musl-arm64.tar.gz" "Linux musl arm64"
 
-try {
-	Invoke-WebRequest -Uri "$($DownloadUrl)dart-sass-$($DartSassVersion)-linux-arm.tar.gz" -OutFile "$TempDir/linux-arm.tar.gz"
-	New-Item -ItemType Directory -Force "./dart-sass/linux-arm"
-	tar -xf "$TempDir/linux-arm.tar.gz" -C "./dart-sass/linux-arm"
-} catch {
-	Write-Host "Problem with Linux arm";
-	Write-Host $_
-}
+downloadAndExtract "macos-x64.tar.gz"        "MacOS x64"
+downloadAndExtract "macos-arm64.tar.gz"      "MacOS arm64"
 
-try {
-	Invoke-WebRequest -Uri "$($DownloadUrl)dart-sass-$($DartSassVersion)-linux-arm64.tar.gz" -OutFile "$TempDir/linux-arm64.tar.gz"
-	New-Item -ItemType Directory -Force "./dart-sass/linux-arm64"
-	tar -xf "$TempDir/linux-arm64.tar.gz" -C "./dart-sass/linux-arm64"
-} catch {
-	Write-Host "Problem with Linux arm64";
-	Write-Host $_
-}
-
-try {
-	Invoke-WebRequest -Uri "$($DownloadUrl)dart-sass-$($DartSassVersion)-macos-x64.tar.gz" -OutFile "$TempDir/macos-x64.tar.gz"
-	New-Item -ItemType Directory -Force "./dart-sass/macos-x64"
-	tar -xf "$TempDir/macos-x64.tar.gz" -C "./dart-sass/macos-x64"
-} catch {
-	Write-Host "Problem with MacOS x64";
-	Write-Host $_
-}
-
-try {
-	Invoke-WebRequest -Uri "$($DownloadUrl)dart-sass-$($DartSassVersion)-macos-arm64.tar.gz" -OutFile "$TempDir/macos-arm64.tar.gz"
-	New-Item -ItemType Directory -Force "./dart-sass/macos-arm64"
-	tar -xf "$TempDir/macos-arm64.tar.gz" -C "./dart-sass/macos-arm64"
-} catch {
-	Write-Host "Problem with MacOS arm64";
-	Write-Host $_
-}
+downloadAndExtract "android-x64.tar.gz"      "Android x64"
+downloadAndExtract "android-ia32.tar.gz"     "Android x86"
+downloadAndExtract "android-arm.tar.gz"      "Android arm"
+downloadAndExtract "android-arm64.tar.gz"    "Android arm64"
 
 Remove-Item -Path $TempDir -Recurse
